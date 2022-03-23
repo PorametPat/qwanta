23fd6adc7156633e0eab90abc1a62b5bd366951b
PorametPat <43396870+PorametPat@users.noreply.github.com>
2022-03-23 17:49:23 +0700

    Add probability circuit in init

--- qwanta/Qubit/__init__.py
+++ qwanta/Qubit/__init__.py
@@ -1,2 +1,2 @@
 from .qubit import PhysicalQubit, LogicalQubit
-from .utils import DirectFidelityEstimator, GetQubit, Qubit, GetQubit_experimental
+from .utils import DirectFidelityEstimator, GetQubit, Qubit, GetQubit_experimental, ProbablyQuantumCircuit
